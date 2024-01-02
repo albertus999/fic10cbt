1760d01f2a203e85d390bd37c8d0d77db3f532c3
albertus999 <albertus_arifin@yahoo.com>
2024-01-02 14:01:22 +0700

    add status ujian, timer

--- app/Http/Controllers/Api/UjianController.php
+++ app/Http/Controllers/Api/UjianController.php
@@ -109,7 +109,19 @@ class UjianController extends Controller
         // dd($soalIds);
 
         $soal = Soal::whereIn('id', $soalIds)->where('kategori', $request->kategori)->get();
-
+        //timer by kategori
+        $timer = $ujian->timer_angka;
+        if ($request->kategori == 'Verbal') {
+            $timer = $ujian->timer_verbal;
+        } else if ($request->kategori == 'Logika') {
+            $timer = $ujian->timer_logika;
+        }
+        return response()->json([
+            'message' => 'Berhasil mendapatkan soal',
+            'timer' => $timer,
+            'data' => SoalResource::collection($soal),
+        ]);
+    }
         return response()->json([
             'message' => 'Berhasil mendapatkan soal',
             'data' => SoalResource::collection($soal),
